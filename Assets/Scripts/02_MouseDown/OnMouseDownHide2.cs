d000a5b8bf8c0b3f14032a17fbfea594261b1f4b
ryoxu1217 <ryousukexiaogu@gmail.com>
2026-06-17 18:57:08 +0900

    ccc

---
+++ Assets/Scripts/02_MouseDown/OnMouseDownHide2.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+// タッチしたら、非表示にする
+public class OnMouseDownHide2 : MonoBehaviour
+{
+    void Update()
+    {
+        // タッチしたら
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            var hit = Physics2D.GetRayIntersection(ray, 100f ,1 << gameObject.layer);
+            if (hit.collider && hit.collider.gameObject == gameObject)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
